7fe151e22d6aa8a754ce0a325445e5eb6d4890df
Xavier Garzon <xavier.garzon@getitllc.com>
2025-02-13 15:36:15 -0500

    little fix

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-# name: discourse-message-plugin
+# name: discourse-plugin-messages-broadcaster
 # about: A plugin to send private messages using a simple form
 # version: 0.1.0
 # authors: Xavier Garzon
-# url: https://github.com/yourusername/discourse-message-plugin
+# url: https://github.com/getit039/discourse-plugin-messages-broadcaster
 
 enabled_site_setting :message_plugin_enabled
 
